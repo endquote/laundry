38c0f09d0b389416fe9ae5bcd65819f8cfcd16d6
Josh Santangelo <josh@endquote.com>
2015-06-04 14:51:07 -0700

    settings for enclosejs

--- enclose.js
+++ enclose.js
@@ -1,4 +1,9 @@
-// enclose --config enclose.js --output laundry index.js
+/* 
+Settings file for use with enclose.js
+Run with this command:
+enclose --config enclose.js --output ./bin/laundry index.js
+*/
+
 module.exports = {
     scripts: "./washers/*.js"
 };
