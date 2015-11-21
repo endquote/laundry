591a5ad82e3cb8f870f717ab6babb4c29a982c8b
Josh Santangelo <josh@endquote.com>
2015-11-20 23:58:02 -0800

    foo

--- items/soundcloud.track.js
+++ items/soundcloud.track.js
@@ -32,7 +32,7 @@ Items.SoundCloud.Track.downloadLogic = function(prefix, obj, washer, cache, down
             var audioSource = util.format('%s?client_id=%s', obj.stream_url, washer.clientId);
 
             // Using ydtl here does get you the original download, but it can often be FLAC or other formats that podcast apps don't like.
-            Storage.downloadUrl(audioSource, target, cache, null, false, download, callback);
+            Storage.downloadUrl(audioSource, target, null, cache, false, download, callback);
         }
     };
 };
