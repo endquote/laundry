bd7a99f66e8dc81a49a2d2610abc65ac3e2ae505
Josh Santangelo <josh@endquote.com>
2016-04-03 13:51:03 -0700

    don't use deprecated instagram API, fixes #10

--- washers/instagram.timeline.js
+++ washers/instagram.timeline.js
@@ -22,7 +22,67 @@ Washers.Instagram.Timeline.prototype = Object.create(Washers.Instagram.prototype
 Washers.Instagram.Timeline.className = Helpers.buildClassName(__filename);
 
 Washers.Instagram.Timeline.prototype.doInput = function(callback) {
-    this.requestMedia('/users/self/feed', callback);
+    // Used to use /users/self/feed, but it got deprectated.
+    // http://developers.instagram.com/post/133424514006/instagram-platform-update
+
+    var that = this;
+    var following = [];
+    var posts = [];
+    var nextUrl;
+
+    // Get all the users you follow.
+    async.doWhilst(function(callback) {
+        var opts = extend({
+            url: nextUrl ? nextUrl : '/users/self/follows'
+        }, that._requestOptions);
+        if (nextUrl) {
+            delete opts.baseUrl;
+        }
+
+        Helpers.jsonRequest(
+            that.job.log,
+            opts,
+            function(response) {
+                following = following.concat(response.data);
+                that.job.log.debug(util.format('Got %d users', following.length));
+                nextUrl = response.pagination.next_url;
+                callback();
+            },
+            callback);
+
+    }, function() {
+        return nextUrl;
+    }, function(err) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        // Get the last page of photos from each user.
+        async.eachLimit(following, 5, function(user, callback) {
+            Helpers.jsonRequest(
+                that.job.log,
+                extend({
+                    url: '/users/' + user.id + '/media/recent'
+                }, that._requestOptions),
+                function(response) {
+                    posts = posts.concat(response.data);
+                    callback();
+                }
+            );
+        }, function(err) {
+            that.job.log.debug('Got %d posts', posts.length);
+
+            // Throw out all but the latest posts.
+            posts.sort(function(a, b) {
+                return b.created_time - a.created_time;
+            });
+            posts = posts.slice(0, 150);
+
+            // Trigger item creation, download, and processing.
+            Item.download(Items.Instagram.Media, that, posts, callback);
+        });
+    });
 };
 
 Washers.Instagram.Timeline.prototype.requestMedia = function(method, callback) {
