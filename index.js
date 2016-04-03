267f5708a1d49e4ed325e139a58f6d7b0e1a50dd
Josh Santangelo <josh@endquote.com>
2016-04-02 19:52:05 -0700

    only update ytdl when running jobs

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ function runCommand() {
         }
 
         // Update youtube-dl every day.
-        if (moment().diff(laundryConfig.settings.ytdlupdate, 'hours') >= 24) {
+        if (cmd === 'run' && moment().diff(laundryConfig.settings.ytdlupdate, 'hours') >= 24) {
             log.info('Updating youtube-dl');
             var downloader = require('youtube-dl/lib/downloader');
             downloader(function error(err, done) {
