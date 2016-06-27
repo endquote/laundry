2926e1e76db479aced91accf465cfecc0dde6060
Josh Santangelo <josh@endquote.com>
2016-06-26 18:46:23 -0700

    new input washer: tumblr likes

---
+++ washers/tumblr.likes.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/*
+Tumblr likes washer
+input: converts liked posts by the Tumblr user into Items
+output: none
+*/
+ns('Washers.Tumblr', global);
+Washers.Tumblr.Likes = function(config, job) {
+    Washers.Tumblr.call(this, config, job);
+
+    this.name = 'Tumblr/Likes';
+    this.className = Helpers.buildClassName(__filename);
+
+    this.input = _.merge(this.input, {
+        description: 'Loads posts you\'ve liked on Tumblr.',
+        prompts: [Washer.downloadMediaOption]
+    });
+};
+
+Washers.Tumblr.Likes.prototype = Object.create(Washers.Tumblr.prototype);
+Washers.Tumblr.Likes.className = Helpers.buildClassName(__filename);
+
+Washers.Tumblr.Likes.prototype.doInput = function(callback) {
+    var quantity = 100;
+    var posts = [];
+    var lastResponse = null;
+    var limit = 20;
+    var that = this;
+    async.doWhilst(function(callback) {
+        // https://www.tumblr.com/docs/en/api/v2
+        Helpers.jsonRequest(
+            that.job.log,
+            extend({
+                uri: '/user/likes',
+                qs: {
+                    limit: Math.min(limit, quantity - posts.length),
+                    offset: posts.length
+                }
+            }, that._requestOptions),
+            function(response) {
+                response = response.response;
+                posts = posts.concat(response.liked_posts);
+                that.job.log.debug(util.format('Got %d/%d posts', posts.length, quantity));
+                lastResponse = response;
+                callback();
+            },
+            callback);
+    }, function() {
+        return lastResponse.liked_posts.length === limit && posts.length < quantity;
+    }, function(err) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        Item.download(Items.Tumblr.Post, that, posts, callback);
+    });
+};
+
+module.exports = Washers.Tumblr.Likes;
