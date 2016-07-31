9b5d91ebfe361b2cd06bdddd7a7c77497b99c266
Josh Santangelo <josh@endquote.com>
2016-07-31 16:52:25 -0700

    instagram 8.5.2 key

--- washers/instagram.js
+++ washers/instagram.js
@@ -23,8 +23,8 @@ Washers.Instagram = function(config, job) {
 
     // https://github.com/mgp25/Instagram-API/blob/master/src/Constants.php
     this._igApi = 'https://i.instagram.com/api/v1/';
-    this._igUserAgent = 'Instagram 8.4.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)';
-    this._igKey = '3d3d669cedc38f2ea7d198840e0648db3738224a0f661aa6a2c1e77dfa964a1e';
+    this._igUserAgent = 'Instagram 8.5.2 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)';
+    this._igKey = '3e0fbc3ab2b2a6b3dbd5f5303dc26cb8d39636291ea9da820283a275d070bd98';
     this._igKeyVersion = '4';
 
     this._jar = request.jar();
