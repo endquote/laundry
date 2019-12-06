10bc3dbbfc0da53cc708aa2b20c47e58017d41b2
Josh Santangelo <josh@endquote.com>
2019-12-06 10:13:39 -0500

    don't commit your password dummy

--- php/instagram-test.js
+++
@@ -1,4 +0,0 @@
-const process = require('child_process');
-const res = process.execSync(`php ${__dirname}/instagram.timeline.php "endquote" "%iH743o>fW7#3N%7"`);
-const json = JSON.parse(res);
-console.log(json);
